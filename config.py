52cfb10d91e2e8d684757cac63367ceb59c4b276
YuvrajSharwa9875 <yuvrajsarva3@gmail.com>
2024-08-13 17:42:07 +0530

    Update config.py 1

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Don't Remove Credit Tg - @Movies_97_bot
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@respect_shots_00
 # Ask Doubt on telegram @KingVJ01
 
 
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 # Bot Information
 API_ID = int(environ.get("API_ID", "20388171"))
 API_HASH = environ.get("API_HASH", "38540844297c567f00a74ad5a09f69b2")
-BOT_TOKEN = environ.get("BOT_TOKEN", "38540844297c567f00a74ad5a09f69b2")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7395577342:AAFkxHyqL9NhaMRXnUj8erJFioEHB-_O2ww")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
@@ -83,7 +83,7 @@ else:
 URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
 
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Don't Remove Credit Tg - @Movies_97_bot
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@respect_shots_00
 # Ask Doubt on telegram @KingVJ01
     
